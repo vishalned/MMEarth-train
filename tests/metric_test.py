a2b693c9c6d18c6f20a1f84644e8dce195663908
Stefan Oehmcke <stefan.oehmcke@gmail.com>
2024-07-16 11:33:41 +0200

    add f1 score test

--- tests/metric_test.py
+++ tests/metric_test.py
@@ -35,10 +35,11 @@ def test_f1_score_alignment_multilabel():
     f1_macro = MultilabelF1Score(num_labels=3, average='macro')
     f1_micro = MultilabelF1Score(num_labels=3, average='micro')
 
-    [f1_macro(y_pred_tensor[[i]], y_true_tensor[[i]]) for i in range(len(y_true))]
-    f1_score_macro_value = f1_macro.compute()
-    [f1_micro(y_pred_tensor[[i]], y_true_tensor[[i]]) for i in range(len(y_true))]
-    f1_score_micro_value = f1_micro.compute()
+    for i in range(5):
+        [f1_macro(y_pred_tensor[[i]], y_true_tensor[[i]]) for i in range(len(y_true))]
+        f1_score_macro_value = f1_macro.compute()
+        [f1_micro(y_pred_tensor[[i]], y_true_tensor[[i]]) for i in range(len(y_true))]
+        f1_score_micro_value = f1_micro.compute()
 
     # Assertions to check alignment
     assert pytest.approx(f1_score_macro_value.item(), 0.001) == expected_f1_macro
